95424a587b4ff35a85df67c314abf12907db102c
Web | Blockchain Enthusiastic <88124724+MetaStar20@users.noreply.github.com>
2022-11-17 19:14:10 +0000

    Update autoBuild.sh

--- autoBuild.sh
+++ autoBuild.sh
@@ -14,13 +14,7 @@ sudo git clone "https://$user_name:$app_password@$repo_path"
 
 sudo apt install npm
 
-sudo apt install curl
-
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-sudo apt install yarn
+sudo npm install -g yarn
 
 cd "/var/www/$project_name/"
 
